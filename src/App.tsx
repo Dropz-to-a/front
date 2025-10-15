b481691c92e0684f4378e4bade913a20e6a69f32
rjsgud49 <qkrrjsgud49@gmail.com>
2025-10-15 17:46:42 +0900

    Initialize repository at project root

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// App.jsx 파일
+import './index.css'; // Import Tailwind CSS styles
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">Hello Tailwind v4 + React!</h1>
+    </div>
+  );
+}
+
+export default App;

1de54f0dca2969e0b73d63a4b0d9ec40fc717068
rlaxogh76 <btm.email2769@gmail.com>
2025-10-15 17:26:25 +0900

    feat: 테스트 코드

--- JOBIT/src/App.tsx
+++ JOBIT/src/App.tsx
@@ -3,10 +3,8 @@ import "./index.css"; // Import Tailwind CSS styles
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 underline">
-        Hello Tailwind v4 + React!
-      </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <img src="https://media.discordapp.net/attachments/1323453523827884126/1395226106038321212/fade_transition.gif?ex=68f05411&is=68ef0291&hm=20ee36453983a4bda89ca072f9318861f781bf2f938fea403121a805c51c0529&=&width=704&height=939" />
     </div>
   );
 }

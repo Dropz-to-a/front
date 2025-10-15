9124697befa7d20a727b46fb44377dadd0183b70
rjsgud49 <qkrrjsgud49@gmail.com>
2025-10-15 17:19:08 +0900

    first seting

---
+++ JOBIT/src/App.tsx
@@ -0,0 +1,14 @@
+// App.jsx 파일
+import "./index.css"; // Import Tailwind CSS styles
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Hello Tailwind v4 + React!
+      </h1>
+    </div>
+  );
+}
+
+export default App;

6da0904e31a7566c4e9bdf94475b59694aa5500a
rlaxogh76 <btm.email2769@gmail.com>
2025-10-29 16:58:18 +0900

    라우팅 설정에 문의하기, 개인정보 처리방침, 이용약관 페이지 추가

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,41 +1,46 @@
-import React from "react";
-import Start from "../pages/Start";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Jobs from "../pages/Jobs";
-import DetailJobs from "../pages/DetailJobs";
-import Profile from "../pages/Profile";
-import Pay from "../pages/Pay";
-import Contracts from "../pages/Contracts";
-import Profileedit from "../pages/Profilleedit";
-import JobCompleted from "../pages/JobCompleted";
-import JobCompletedAdmin from "../pages/JobCompletedAdmin";
-import MyApplications from "../pages/MyApplications";
+import React from 'react'
+import Start from '../pages/Start'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Jobs from '../pages/Jobs'
+import DetailJobs from '../pages/DetailJobs'
+import Profile from '../pages/Profile'
+import Pay from '../pages/Pay'
+import Contracts from '../pages/Contracts'
+import Profileedit from '../pages/Profilleedit'
+import JobCompleted from '../pages/JobCompleted'
+import JobCompletedAdmin from '../pages/JobCompletedAdmin'
+import MyApplications from '../pages/MyApplications'
+import Contact from '../pages/Contact'
+import Privacy from '../pages/Privacy'
+import Terms from '../pages/Terms'
 
 export type RouteItem = {
-    path: string;
-    element: React.ReactNode;
-};
+  path: string
+  element: React.ReactNode
+}
 
 export const publicRoutes: RouteItem[] = [
-    { path: "/", element: <Home /> },
-    { path: "/start", element: <Start /> },
-    { path: "/login", element: <Login /> },
-    { path: "/register", element: <Register /> },
-    { path: "/about", element: <About /> },
-    { path: "/jobs", element: <Jobs /> },
-    { path: "/jobs/:id", element: <DetailJobs /> },
-    { path: "/profile", element: <Profile /> },
-    { path: "/Profilleedit", element: <Profileedit /> },
-    { path: "/pay", element: <Pay /> },
-    { path: "/contracts", element: <Contracts /> },
-    { path: "/jobs/:id/completed", element: <JobCompleted /> },
-    { path: "/jobs/:id/completed/admin", element: <JobCompletedAdmin /> },
-    { path: "/my-applications", element: <MyApplications /> },
-];
+  { path: '/', element: <Home /> },
+  { path: '/start', element: <Start /> },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
+  { path: '/about', element: <About /> },
+  { path: '/jobs', element: <Jobs /> },
+  { path: '/jobs/:id', element: <DetailJobs /> },
+  { path: '/profile', element: <Profile /> },
+  { path: '/Profilleedit', element: <Profileedit /> },
+  { path: '/pay', element: <Pay /> },
+  { path: '/contracts', element: <Contracts /> },
+  { path: '/jobs/:id/completed', element: <JobCompleted /> },
+  { path: '/jobs/:id/completed/admin', element: <JobCompletedAdmin /> },
+  { path: '/my-applications', element: <MyApplications /> },
+  // Footer 페이지
+  { path: '/contact', element: <Contact /> },
+  { path: '/privacy', element: <Privacy /> },
+  { path: '/terms', element: <Terms /> },
+]
 
-export const privateRoutes: RouteItem[] = [
-    { path: "/", element: <Home /> },
-];
+export const privateRoutes: RouteItem[] = [{ path: '/', element: <Home /> }]

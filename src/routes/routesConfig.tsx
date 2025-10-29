eddca4da053e3bf469dd17cd4f0ffceb86b50134
rjsgud49 <qkrrjsgud49@gmail.com>
2025-10-29 13:25:20 +0900

    라우팅설정

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -11,6 +11,8 @@ import Pay from "../pages/Pay";
 import Contracts from "../pages/Contracts";
 import Profileedit from "../pages/Profilleedit";
 import JobCompleted from "../pages/JobCompleted";
+import JobCompletedAdmin from "../pages/JobCompletedAdmin";
+import MyApplications from "../pages/MyApplications";
 
 export type RouteItem = {
     path: string;
@@ -30,6 +32,8 @@ export const publicRoutes: RouteItem[] = [
     { path: "/pay", element: <Pay /> },
     { path: "/contracts", element: <Contracts /> },
     { path: "/jobs/:id/completed", element: <JobCompleted /> },
+    { path: "/jobs/:id/completed/admin", element: <JobCompletedAdmin /> },
+    { path: "/my-applications", element: <MyApplications /> },
 ];
 
 export const privateRoutes: RouteItem[] = [

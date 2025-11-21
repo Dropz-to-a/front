49b89cf606c4f801717755fccf91fbc025b57d7a
rjsgud49 <qkrrjsgud49@gmail.com>
2025-11-21 19:20:02 +0900

    불필요한 import제거

--- src/pages/jobs/company/JobEdit.tsx
+++ src/pages/jobs/company/JobEdit.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Header from "../../../components/Header";
-import type { Job } from "../user/Jobs";
 
 export default function JobEdit() {
     const navigate = useNavigate();

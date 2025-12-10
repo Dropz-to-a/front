97e5c9695bfe0b6b7ff853043c2ef078f64966b9
rjsgud49 <qkrrjsgud49@gmail.com>
2025-12-10 14:57:25 +0900

    fix: 이미지 경로 외부로 수정

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,12 +3,13 @@ import type { FC } from 'react'
 import AuthBackground from '@/components/AuthBackground'
 import LoginForm from '@/components/LoginForm'
 import AuthIntro from '@/components/AuthIntro'
+import JobitLogo from '../../public/logo(white).svg'
 
 const Login: FC = () => {
   return (
     <AuthBackground>
       <AuthIntro
-        img={'./public/logo(white).svg'}
+        img={JobitLogo}
         title={'만나서 반가워요!'}
         description={'당신이 누군지 저희에게 알려주세요!'}
       />

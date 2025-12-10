95b1c545f019ce5bc51de7bd9950bd6c801104ed
rjsgud49 <qkrrjsgud49@gmail.com>
2025-12-10 15:10:12 +0900

    fix: 이미지 경로 정정
    태그 내부에서  /바로 쓰던가 아니면
    에셋 파일에 넣고 임포트하던가

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,13 +3,12 @@ import type { FC } from 'react'
 import AuthBackground from '@/components/AuthBackground'
 import LoginForm from '@/components/LoginForm'
 import AuthIntro from '@/components/AuthIntro'
-import JobitLogo from '../../public/logo(white).svg'
 
 const Login: FC = () => {
   return (
     <AuthBackground>
       <AuthIntro
-        img={JobitLogo}
+        img={'/logo(white).svg'}
         title={'만나서 반가워요!'}
         description={'당신이 누군지 저희에게 알려주세요!'}
       />

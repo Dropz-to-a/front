52d192b66687db5a9fe779f6367e6c69b73504cb
Qlellow <changyeon0702@gmail.com>
2025-12-10 16:14:52 +0900

    fix: 로그인 및 회원가입 이미지 url 수정

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -3,12 +3,13 @@ import type { FC } from 'react'
 import AuthBackground from '@/components/AuthBackground'
 import RegisterForm from '@/components/RegisterForm'
 import AuthIntro from '@/components/AuthIntro'
+import JobitLogo from '@/../public/logo(white).svg'
 
 const Register: FC = () => {
   return (
     <AuthBackground>
       <AuthIntro
-        img={'./public/logo(white).svg'}
+        img={JobitLogo}
         title={'만나서 반가워요!'}
         description={'당신이 누군지 저희에게 알려주세요!'}
       />

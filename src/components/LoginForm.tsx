c4143bad3c915228782cc868f6f511412c31a8d8
rlaxogh76 <btm.email2769@gmail.com>
2025-12-19 10:50:55 +0900

    feat: 로그인 및 온보딩 과정에서 토스트 메시지 추가 및 알림 개선

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -40,11 +40,9 @@ const LoginForm: FC = () => {
       toastShownRef.current = true
       showSuccessToast('로그인에 성공했습니다!')
 
-      setTimeout(() => {
-        navigate(userType === 'company' ? '/company/onboarding' : '/user/onboarding', {
-          replace: true,
-        })
-      }, 2000)
+      navigate(userType === 'company' ? '/company/onboarding' : '/user/onboarding', {
+        replace: true,
+      })
     } else {
       toastShownRef.current = true
       showErrorToast('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.')

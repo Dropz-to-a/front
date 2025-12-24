3381807c91f141f1c1095f793206aab02e71dd4c
Qlellow <changyeon0702@gmail.com>
2025-12-24 21:41:24 +0900

    fix: Error타입 수정

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -37,7 +37,7 @@ let failedQueue: Array<{
   reject: (reason?: unknown) => void
 }> = []
 
-const processQueue = (error: AxiosError | null, token: string | null = null) => {
+const processQueue = (error: Error | AxiosError | null, token: string | null = null) => {
   failedQueue.forEach(prom => {
     if (error) {
       prom.reject(error)
